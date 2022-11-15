569703cd08fae68066ab7208415e980463c962bb
jaysmith13 <janeasmith13@gmail.com>
2022-11-15 01:10:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-pa
+require('dotenv').config();
+
+const fs = require('fs');
+const {Client, collection, Intents} = require('discord.js');
+
+const token = process.env['QR_BOT_TOKEN'];
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
