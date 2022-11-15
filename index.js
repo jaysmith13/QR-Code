9136aeda0be08bf941fd9f4053f35b74779a4c94
jaysmith13 <janeasmith13@gmail.com>
2022-11-15 01:14:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const {Client, collection, Intents} = require('discord.js');
 const token = process.env['QR_BOT_TOKEN'];
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
+client.commands = new collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require('./commands/${file}');
+
+    client.commands.set(command.data.name, command);
+}
