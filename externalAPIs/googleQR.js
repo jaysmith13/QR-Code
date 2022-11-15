bec830e288f9c774453bbd7c233512e8e141e293
jaysmith13 <janeasmith13@gmail.com>
2022-11-15 01:33:02 -0700

    Update googleQR.js

--- externalAPIs/googleQR.js
+++ externalAPIs/googleQR.js
@@ -2,6 +2,9 @@ const googleAPIURL = 'https://chart.googleapis.com/chart?cht=qr';
 
 module.exports = {
     generateQR(data, height, width, color){
-        if(!data)
-    }
+        if(!data) {
+            throw new Error('Please provide a valid URL');
+        }
+        return `${googleAPIURL}&chl="${data}"${height && width ? "&chs=" + height + "x" + width :"&chs=200x200"}${color?"&chco=" + color :""}`;
+    } 
 }
