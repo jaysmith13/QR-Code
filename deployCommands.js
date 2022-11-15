4ea0f4b9f8e0ac5f6c182f8c02d1801e35abd4f7
jaysmith13 <janeasmith13@gmail.com>
2022-11-15 01:37:31 -0700

    Update deployCommands.js

--- deployCommands.js
+++ deployCommands.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const fs = require('fs');
 const token = process.env['QR_BOT_TOKEN'];
 const clientId = process.env['CLIENT_ID'];
-const guildId = ;
+const guildId = process.env['GUILD_ID'];
 
 const {} = ;
 const {REST} = ;
